abc4f774f8eb70c48b20c10243e78a3b37238281
Scott <scottgrimes89@gmail.com>
2018-06-29 10:00:54 -0500

    Refactor for all generations

--- Search.js
+++ Search.js
@@ -1,82 +1,12 @@
 
 
-class Search extends React.Component {
+const Search = (props)=> (<div className="search-bar form-inline">
+  <SearchPokemon namesList={props.namesList} fetchByName={props.fetchByName} />
+  <select>{Object.keys(props.generations).map((game, i)=> (<option key = {i} value = {game}>{game}</option> ) ) }</select>
+</div>);
 
-  constructor(props){
-    super(props);
-    this.state = {value:'',suggestions:[], namesList:props.namesList}
-
-    this.onChange = this.onChange.bind(this);
-    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
-    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
-    this.getSuggestions = this.getSuggestions.bind(this);
-
-  }
-
-  onChange(event, {newValue}){
-    this.setState({value:newValue})
-  }
-
-  onSuggestionsFetchRequested({value}){
-    this.setState({
-      suggestions: this.getSuggestions(value)
-    });
-  }
-
-  onSuggestionsClearRequested(){
-    this.setState({suggestions:[]})
-  }
-
-  getSuggestions(value){
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-    console.log(inputValue,inputLength)
-    return inputLength === 0? [] : this.state.namesList.filter(name=>{
-      return name.slice(0,inputLength) === inputValue
-    });
-  }
-
-  renderSuggestion(suggestion){
-    return(<div>{suggestion}</div>)
-  }
-
-  submitHandler(e){
-    e.preventDefault();
-    this.props.fetchByName(this.state.value);
-  }
-
-  render(){
-
-    const {value, suggestions} = this.state;
-
-    const inputProps = {
-      placeholder: 'Search for a pokemon...',
-      value:this.state.value,
-      onChange: this.onChange
-    }
-
-//<input className="form-control" type="text" />
-    return(
-    <div className="search-bar form-inline">
-    <form className ="search-form" onSubmit = {this.submitHandler.bind(this)}>
-      <Autosuggest
-        suggestions = {suggestions}
-        onSuggestionsClearRequested = {this.onSuggestionsClearRequested}
-        onSuggestionsFetchRequested = {this.onSuggestionsFetchRequested}
-        getSuggestionValue = {(x)=>x}
-        renderSuggestion = {this.renderSuggestion}
-        inputProps = {inputProps}
-      />
-      <submit className="btn hidden-sm-down">
-        <span className="glyphicon glyphicon-search"></span>
-      </submit>
-      </form>
-    </div> 
-  );
-}; 
-}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // var declarations will only exist globally where explicitly defined
- window.Search = Search;
+window.Search = Search;
 
--- app.js
+++ app.js
@@ -4,69 +4,90 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      ids:{},
-      selected:{},
-      names:[]
-    }
+      ids: {},
+      selected: {},
+      names: [],
+      versionName: 'yellow',
+      generations: window.generations
+    };
     this.fetchByName = this.fetchByName.bind(this);
+    this.updateGameSelection = this.updateGameSelection.bind(this);
   }
 
-  componentWillMount() {
-    const charizard = {"name":"charizard","image":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png","types":["flying","fire"],"flavorText":"When expelling a blast of super hot fire, the red flame at the tip of its tail burns more intensely."}
-    this.setState({selected : charizard, ids:window.defaultIDs, names: window.defaultNames})
+  componentWillMount() {    
+    const list = window.defaultList;
+    this.setState({selected: window.defaultPokemon, ids: idsFromList(list), names: namesFromList(list)});
   }
 
-  fetchByName(name){
+  updateGameSelection(name) {
+    this.setState({versionName: name},
+      fetchPokemon(name)
+        .then(list => {
+          this.setState({ ids: idsFromList(list), names: namesFromList(list) },
+            () => {this.fetchByName(this.state.selected.name)}
+          );
+        });
+    );
     
-    if(!this.state.names.includes(name)){
-      alert('Pokemon Not Found')
-      return
-    }
 
-    //$('.form-control').val('');
-    this.setState({selected:{}})
+  }
+
+  fetchByName(name) {
+    
+    if (!this.state.names.includes(name)) {
+      alert('Pokemon Not Found');
+      return;
+    }
+    this.setState({selected: {}});
     window.fetchPokemon( this.state.ids[name] )
-    .then(response =>{
-      if(response.detail && response.detail==='Not Found'){console.log('Not Found')}
-      this.setState({selected: response});
-    })
+      .then(response =>{
+        if (response.detail && response.detail === 'Not Found') { console.log('Not Found'); }
+        this.setState({selected: response});
+      });
   }
 
 
-  render(){
-    return(
-    <div>
-      <nav className="navbar">
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <Search namesList={this.state.names} fetchByName={this.fetchByName}/> 
+            <Search generations={this.state.generations} namesList={this.state.names} updateGameSelection={this.updateGameSelection} fetchByName={this.fetchByName}/> 
           </div>
-      </nav>
-      <div className="row">
-      <div className="col-md-5">
-        <PokeCenter pokemon={this.state.selected}/>
-      </div>
-      </div>
-    </div>);
+        </nav>
+        <div className="row">
+          <div className="col-md-5">
+            <PokeCenter pokemon={this.state.selected}/>
+          </div>
+        </div>
+      </div>);
   }
 
 }
-
-const versionName = 'yellow';
 const languageName = 'en';
 
 
 
+const idsFromList = function idsFromList(pokemonList) {
+  return pokemonList.reduce((obj, x) => {
+    obj[x.name] = x.id;
+    return obj;
+  }, {});
+};
+
+const namesFromList = function namesFromList(pokemonList) {
+  return pokemonList.map(x => x.name);
+};
 
 
+const fetchPokemonList = function fetchPokemonList() {
+  return new Promise(function(resolve, reject) {
 
-const fetchPokemonList = function fetchPokemonList(generation) {
-  return new Promise(function(resolve,reject){
-
-    generation = 1;
+    const generation = generationNumbers[this.state.versionName];
     const idOf = (x) => x.url.replace('https://pokeapi.co/api/v2/pokemon-species/', '').replace('/', '');
 
     fetch(`https://pokeapi.co/api/v2/generation/${generation}`)
-      .then(result   => result.json())
+      .then(result => result.json())
       .then(x => {
         return x.pokemon_species.map(el => {
           return { id: idOf(el), name: el.name };
@@ -74,30 +95,30 @@ const fetchPokemonList = function fetchPokemonList(generation) {
       })
       .then( x => { resolve(x); });
 
-  })
+  });
   
-}
+};
 
-const fetchPokemon = function fetchPokemon(id){
+const fetchPokemon = function fetchPokemon(id) {
 
-  return new Promise(function(resolve,reject){
+  return new Promise(function(resolve, reject) {
     var pokemon = {};
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((x) => x.json())
-    .then((x) => {
-      console.log(x);
-      pokemon.name = x.name;
-      pokemon.image = x.sprites.front_default;
-      pokemon.types = x.types.map(x => x.type.name);
-      return fetch(x.species.url)
-    })
-    .then(speciesInfo => speciesInfo.json() )
-    .then(speciesInfo => {
-      console.log(speciesInfo)
-      pokemon.flavorText = speciesInfo.flavor_text_entries.filter((x) => x.language.name === 'en' && x.version.name === 'yellow')[0].flavor_text;
-      resolve(pokemon);
-    });
+      .then((x) => x.json())
+      .then((x) => {
+        console.log(x);
+        pokemon.name = x.name;
+        pokemon.image = x.sprites.front_default;
+        pokemon.types = x.types.map(x => x.type.name);
+        return fetch(x.species.url);
+      })
+      .then(speciesInfo => speciesInfo.json() )
+      .then(speciesInfo => {
+        console.log(speciesInfo);
+        pokemon.flavorText = speciesInfo.flavor_text_entries.filter((x) => x.language.name === 'en' && x.version.name === 'yellow')[0].flavor_text;
+        resolve(pokemon);
+      });
 
   });
   
@@ -109,4 +130,4 @@ window.App = App;
 window.fetchPokemon = fetchPokemon;
 window.fetchPokemonList = fetchPokemonList;
 
-ReactDOM.render( <App />,document.getElementById('app'));
+ReactDOM.render( <App />, document.getElementById('app'));
